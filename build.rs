9122076733ba503da6b00845f33bb2f21a471f31
aw7hx5do2aisvoh9.gitmask@pay.pizza <aw7hx5do2aisvoh9.gitmask@pay.pizza>
2021-07-17 15:22:04 +0200

    disable protobuf output formatting

--- build.rs
+++ build.rs
@@ -2,7 +2,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::configure()
         .build_client(true)
         .build_server(false)
-        .format(true)
         .compile(&["proto/health.proto"], &["proto"])
         .map_err(|err| err.into())
 }
